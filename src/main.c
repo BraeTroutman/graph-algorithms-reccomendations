8fed5bce693e01197a337c60d5fa45d25ba894eb
BraeTroutman <braetroutman@gmail.com>
2023-02-21 17:57:37 -0500

    remove excessive prints

--- src/main.c
+++ src/main.c
@@ -7,10 +7,9 @@ int main(int argc, char* argv[]) {
 
 	am_graph_read("data/A4-edge.txt", &graph);
 
-	am_graph_print(graph);
-	
 	am_graph paths;
 	am_graph_2paths(graph, &paths);
+	puts("ALL TWO-PATHS");
 	am_graph_print(paths);
 
 	return 0;
