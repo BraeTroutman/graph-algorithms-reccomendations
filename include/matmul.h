d6036883653a13354fd9a0c308df32acfe846348
BraeTroutman <braetroutman@gmail.com>
2023-02-20 20:37:07 -0500

    move matmul procedures to different file

---
+++ include/matmul.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+void matmul(int* A, int* B, int* C, int N);
+void strassens(int* A, int* B, int* C, int N);
+static void addmat(int* a, int* b, int* c, int n);
+static void submat(int* a, int* b, int* c, int n);
+
