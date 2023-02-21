7e5d3d4623c3e8116edbb133dd71195e097d52cd
BraeTroutman <braetroutman@gmail.com>
2023-02-21 16:08:33 -0500

    rename matmul to matrix

--- include/matmul.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-void matmul(int* A, int* B, int* C, int N);
-void strassens(int* A, int* B, int* C, int N);
-static void addmat(int* a, int* b, int* c, int n);
-static void submat(int* a, int* b, int* c, int n);
-
