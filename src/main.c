a79b2c827ed96ed1dcd4d81484d591da51f1f118
BraeTroutman <braetroutman@gmail.com>
2023-02-21 17:47:02 -0500

    change graph source and calculate 2paths

--- src/main.c
+++ src/main.c
@@ -5,9 +5,13 @@
 int main(int argc, char* argv[]) {
 	am_graph graph;
 
-	am_graph_read("data/A16-edge.txt", &graph);
+	am_graph_read("data/A4-edge.txt", &graph);
 
 	am_graph_print(graph);
+	
+	am_graph paths;
+	am_graph_2paths(graph, &paths);
+	am_graph_print(paths);
 
 	return 0;
 }
