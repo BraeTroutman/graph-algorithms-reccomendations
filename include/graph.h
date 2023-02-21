578755ae68e6862f657946b5136840467a0bfe8c
BraeTroutman <braetroutman@gmail.com>
2023-02-21 17:46:04 -0500

    add signature for 2path function

--- include/graph.h
+++ include/graph.h
@@ -10,4 +10,5 @@ typedef struct {
 void am_graph_init(am_graph *graph, int m, int n);
 void am_graph_read(const char *filename, am_graph* graph);
 void am_graph_print(am_graph graph);
+void am_graph_2paths(am_graph graph, am_graph *result);
 
