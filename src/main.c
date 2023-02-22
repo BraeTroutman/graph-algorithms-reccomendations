902949b959aeb9ce14b566721725963f43b8c22e
BraeTroutman <braetroutman@gmail.com>
2023-02-22 11:17:36 -0500

    read file as both am and al, display graphs

--- src/main.c
+++ src/main.c
@@ -3,14 +3,19 @@
 #include "graph.h"
 
 int main(int argc, char* argv[]) {
-	am_graph graph;
+	am_graph am;
+	am_graph_read("data/A4-edge.txt", &am);
+	am_graph_print(am);
 
-	am_graph_read("data/A4-edge.txt", &graph);
-
+	puts("\tALL TWO-PATHS");
 	am_graph paths;
-	am_graph_2paths(graph, &paths);
-	puts("ALL TWO-PATHS");
+	am_graph_2paths(am, &paths);
 	am_graph_print(paths);
+	
+	puts("");
+	al_graph graph2;
+	al_graph_read("data/A4-edge.txt", &graph2);
+	al_graph_print(graph2);
 
 	return 0;
 }
