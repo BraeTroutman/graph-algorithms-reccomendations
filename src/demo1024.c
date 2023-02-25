4c4ddf9f01363ca539271c0da36b5be381330340
BraeTroutman <braetroutman@gmail.com>
2023-02-25 18:02:54 -0500

    add fns for 2paths output

--- src/demo1024.c
+++ src/demo1024.c
@@ -4,6 +4,44 @@
 #include "graph.h"
 #include "words.h"
 
+void calculate_reccomendations_exclusive(al_graph paths, al_graph original) {
+	FILE *out = fopen("data/out/exclusive/rec1024.txt", "w");
+	
+	int i, j;
+
+	for (i = 0; i < paths.n; i++) {
+		int max_weight = 0;
+		int max_edge = -1;
+		int src = paths.nodes[i];
+
+		for (j = 0; j < paths.num_edges[i]; j++) {
+			int dst = paths.edges[src+j];
+			if (al_graph_edge_exists(original, i, dst) || i == dst) continue;
+			if (paths.weights[src+j] > max_weight) {
+				max_weight = paths.weights[src+j];
+				max_edge = dst;
+			}
+		}
+		fprintf(out, "%d\n", max_edge+1);	
+	}
+
+	fclose(out);
+}
+
+void calculate_reccomendations_inclusive(al_graph paths) {
+	FILE *out = fopen("data/out/inclusive/rec1024.txt", "w");
+	
+	int *reccs = calloc(paths.n, sizeof(int));
+	al_graph_heaviest_edges(paths, reccs, 0);
+
+	int i; 
+	for (i = 0; i < paths.n; i++) {
+		fprintf(out, "%d\n", reccs[i]+1);
+	}
+
+	fclose(out);
+}
+
 int main(int argc, char* argv[]) {
 	clock_t start, mat_time, list_time;
 
@@ -14,13 +52,13 @@ int main(int argc, char* argv[]) {
 	puts("reading to alist");
 	al_graph al;
 	al_graph_read("data/A1024-edge.txt", &al);
-	al_graph_print(al);
+	//al_graph_print(al);
 	
 	puts("\ncalculating 2paths amatrix");
 	start = clock();
 	puts("\nadjacency matrix two-paths");
 	am_graph am_paths;
-	am_graph_2paths(am, &am_paths);
+	//am_graph_2paths(am, &am_paths);
 	mat_time = clock() - start;
 	printf("took %f seconds\n", ((float) mat_time)/CLOCKS_PER_SEC);
 
@@ -33,6 +71,9 @@ int main(int argc, char* argv[]) {
 
 	printf("The adjacency list algorithm was %f times faster\n", 
 			((float)mat_time / CLOCKS_PER_SEC) / ((float) list_time / CLOCKS_PER_SEC));	
+	
+	calculate_reccomendations_exclusive(al_paths, al);
+	calculate_reccomendations_inclusive(al_paths);
 
 	return 0;
 }
